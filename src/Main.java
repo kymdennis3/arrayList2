d65f9faeb39c73871baa4b333f18877cec3b3d39
Kimtai <kymdennis3@gmail.com>
2023-01-13 07:00:17 +0300

    displaying the array list using for()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<String> food = new ArrayList<>();
+
+        food.add("Pizza");
+        food.add("Hamburger");
+        food.add("Hot dog");
+
+        for(int i = 0; i<food.size(); i++){
+            System.out.println(food.get(i));
+        }
     }
 }
